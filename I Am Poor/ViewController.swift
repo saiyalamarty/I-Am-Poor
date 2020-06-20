5c7d755fd109e0fedc00118c88f4d04709000a09
Sai Yalamarty <sai1494@gmail.com>
2020-06-20 01:31:46 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sai Sharan Yalamarty on 6/20/20.
+//  Copyright © 2020 Sai Sharan Yalamarty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
